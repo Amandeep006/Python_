aaffcb4cfb70f406b216d2fe3aea8a0b0636733c
Amandeep <amandeepsaini951@gmail.com>
2026-06-15 23:48:46 +0530

    sixth commit

--- PyhtonQues/basic.py
+++ PyhtonQues/basic.py
@@ -3,23 +3,23 @@
 2.  Performs the basic mathematical operations on these two numbers:
 """
 
-# num1=int(input("Enter your first number :"))
-# num2=int(input("Enter your second number :"))
-# # Addition 
-# sum=num1+num2
-# print(f"The addition of {num1} & {num2} : {sum}")
+num1=int(input("Enter your first number :"))
+num2=int(input("Enter your second number :"))
+# Addition 
+sum=num1+num2
+print(f"The addition of {num1} & {num2} : {sum}")
 
-# # Subtraction 
-# sub=num1-num2
-# print(f"The subtraction of {num1} & {num2} : {sub}")
+# Subtraction 
+sub=num1-num2
+print(f"The subtraction of {num1} & {num2} : {sub}")
 
-# # Multipication
-# mult=num1*num2
-# print(f"The Multipilcation of {num1} & {num2} : {mult}")
+# Multipication
+mult=num1*num2
+print(f"The Multipilcation of {num1} & {num2} : {mult}")
 
-# # Division 
-# div=num1//num2
-# print(f"The divison of {num1} & {num2} : {div}")
+# Division 
+div=num1//num2
+print(f"The divison of {num1} & {num2} : {div}")
 
 
 
@@ -29,12 +29,12 @@
 3.  Prints a personalized greeting message using the full name.
 """
 
-# first_name=input("Enter your first name :")
-# last_name=input("Enter your last name :")
+first_name=input("Enter your first name :")
+last_name=input("Enter your last name :")
 
-# full_name=f"{first_name} {last_name}"
+full_name=f"{first_name} {last_name}"
 
-# print(f"Hello {full_name}!, Welcome to the python world. Hope your journey is going pleasant and enjoyable.")
+print(f"Hello {full_name}!, Welcome to the python world. Hope your journey is going pleasant and enjoyable.")
 
 
 
@@ -51,3 +51,84 @@ elif (n%2==0 and n>6 and n<20):
 
 else :
     print("Not weird ")
+
+
+"""
+Write a Python program that:
+1. 	Takes an integer input from the user.
+2. 	Checks whether the number is even or odd using an if-else statement.
+3. 	Displays the result accordingly.
+
+"""
+
+num= "\nAnytime you want to quit just enter 'q'. "
+num+="\nEnter any number: "
+
+while True:
+    if num=='q':
+        break
+
+    else:
+        n1=int(input(num))
+        if n1%2==0:
+            print(f"The number {n1} is even.")
+        else:
+            print(f"The number {n1} is odd.")
+
+
+                   
+"""
+ Write a Python program that:
+1.   Uses a for loop to iterate over numbers from 1 to 50.
+2.   Calculates the sum of all integers in this range.
+3.   Displays the final sum.
+
+"""
+sum=0
+for i in range(1,51):
+    sum+=i
+
+print(f"THe sum of all numbers between 1 to 50: {sum}" )
+    
+
+
+"""
+ Write a Python program that:
+1.   Defines a function named factorial that takes a number as an argument and calculates its factorial using a loop or recursion.
+2.   Returns the calculated factorial.
+3.   Calls the function with a sample number and prints the output.
+
+"""
+
+def factorial(n):
+    fact=1
+    while(n>0):
+        fact=n*fact
+        n=n-1
+
+    return fact
+
+num=int(input("Enter any number :"))
+print(f"The factorial of {num} is {factorial(num)}")
+
+
+"""
+Write a Python program that:
+1.   Asks the user for a number as input.
+2.   Uses the math module to calculate the:
+o   Square root of the number
+o   Natural logarithm (log base e) of the number
+o   Sine of the number (in radians)
+3.   Displays the calculated results.
+
+"""
+import math
+num=int(input("Enter you any number :"))
+sqr=math.sqrt(num)
+lgn=math.log(num)
+rad=math.radians(num)
+snum=math.sin(rad)
+
+print(f"The Square root of {num} is {sqr}")
+print(f"The logarithm of {num} is {lgn}")
+print(f"The Sine of {num} is {snum}")
